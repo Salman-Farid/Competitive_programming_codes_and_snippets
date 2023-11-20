754062d3a3b5fe2d23790fdaafae72afdcb6eb84
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2023-11-20 20:10:14 +0600

    problem - 75

---
+++ Codeforces codes/A. Even Odds.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    long long n, k; cin >> n >> k;
+    if (k <= (n+1) / 2) cout << 2 * k - 1 << endl;
+    else  cout << 2 * (k - (n+1) / 2) << endl;
+}

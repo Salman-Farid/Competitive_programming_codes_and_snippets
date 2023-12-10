268879ab3befc39457c37abd2e6b4456d4678ec6
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2023-12-10 19:00:49 +0600

    update

--- Leetcode codes/Two sum.cpp
+++ Leetcode codes/Two sum.cpp
@@ -17,3 +17,58 @@ public:
         return {};
     }
 };
+
+//option : 2
+// And be patient. Allah will not waste the reward of the virtuous.
+// #include <vector>
+// #include <unordered_map>
+
+// class Solution
+// {
+// public:
+//     vector<int> twoSum(vector<int> &nums, int target)
+//     {
+//         int output[2];
+//         for (int l = 0; l < nums.size(); l++)
+//         {
+//             for (int j = l + 1; j < nums.size(); j++) // Use j = i + 1 to avoid comparing elements with themselves.
+//             {
+//                 if (nums[l] + nums[j] == target)
+//                 {
+//                     output[0]=l;
+//                     output[1]=j;
+                    
+//                 }
+//             }
+//         }
+//         return {output[0],output[1]};
+//     }
+// };
+
+//most optimized way:
+// class Solution {
+
+//  public:
+
+//   vector<int> twoSum(vector<int>& nums, int target) {
+
+//     unordered_map<int, int> numToIndex;
+
+//     for (int i = 0; i < nums.size(); ++i) {
+
+//       if (const auto it = numToIndex.find(target - nums[i]);
+
+//           it != numToIndex.cend())
+
+//         return {it->second, i};
+
+//       numToIndex[nums[i]] = i;
+
+//     }
+
+//     throw;
+
+//   }
+
+// };
+

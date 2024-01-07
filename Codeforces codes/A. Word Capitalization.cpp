23d85c55111caa0f9e02586a1f0d164f23f6f0e8
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-01-07 17:40:30 +0600

    problem : 120

---
+++ Codeforces codes/A. Word Capitalization.cpp
@@ -0,0 +1,26 @@
+//Option: 1
+// And be patient. Allah will not waste the reward of the virtuous. 
+//اللَّهُ أَكْبَرُ
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s,p; cin >> s;
+    p= toupper(s[0]);
+    s.replace(0,1,p);
+    cout << s;
+}
+
+//Option: 2
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     string s,p,q,l,m; cin >> s;
+//     q=s;
+//     l=s.erase(0,1);
+//     m=toupper(q[0]);
+//     p= m+l;
+//     cout<<p;
+// }

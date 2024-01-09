e0d66486173856decab03ac1ed7998ecca44f394
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-01-10 01:12:14 +0600

    Problem:132

--- Codeforces codes/A. The New Year Meeting Friends.cpp
+++ Codeforces codes/A. The New Year Meeting Friends.cpp
@@ -24,3 +24,15 @@ int main()
 //     } 
 //     cout<<p-q;
 // }
+
+//Option 3:
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a[3];
+//     for(int i=0;i<3;i++){
+//         cin>>a[i];
+//     }
+//     sort(a,a+3);
+//     cout<<a[2]-a[0]<<endl;
+// }

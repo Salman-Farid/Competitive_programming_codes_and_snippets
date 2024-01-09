4330b1d87714936ed7eea5bf7c5a1e0f6e43d279
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-01-10 01:13:50 +0600

    Problem: 132

--- Codeforces codes/A. The New Year Meeting Friends.cpp
+++ Codeforces codes/A. The New Year Meeting Friends.cpp
@@ -1,14 +1,12 @@
 // And be patient. Allah will not waste the reward of the virtuous.
 // اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int arr[3],c=0;
-    for (int i = 0; i < 3; i++) cin >> arr[i];
-    sort(arr, arr + 3);
-    for (int j = 0; j < 2; j++) c+= (arr[j+1] - arr[j]);
-    cout<<c;
+int main(){
+    int a[3];
+    for(int i=0;i<3;i++) cin>>a[i];
+    sort(a,a+3);
+    cout<<a[2]-a[0];
 }
 
 //Option:2
@@ -26,13 +24,13 @@ int main()
 // }
 
 //Option 3:
-// #include<bits/stdc++.h>
+// #include <bits/stdc++.h>
 // using namespace std;
-// int main(){
-//     int a[3];
-//     for(int i=0;i<3;i++){
-//         cin>>a[i];
-//     }
-//     sort(a,a+3);
-//     cout<<a[2]-a[0]<<endl;
+// int main()
+// {
+//     int arr[3],c=0;
+//     for (int i = 0; i < 3; i++) cin >> arr[i];
+//     sort(arr, arr + 3);
+//     for (int j = 0; j < 2; j++) c+= (arr[j+1] - arr[j]);
+//     cout<<c;
 // }

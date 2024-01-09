37ea835c85c30bbef167fc9ee14b5bd934e42854
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-01-10 00:23:20 +0600

    Problem: 131

--- Codeforces codes/A. Fox And Snake.cpp
+++ Codeforces codes/A. Fox And Snake.cpp
@@ -20,3 +20,37 @@ int main()
         cout<<endl;
     }
 }
+
+// Option: 2
+// And be patient. Allah will not waste the reward of the virtuous.
+// اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n, m, y = 3; bool t = false; cin >> n >> m;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             if (i % 2 == 0) cout << "#";
+//             else
+//             {
+//                 if (i == y)
+//                 {
+//                     y += 4;
+//                     t = true;
+//                     if (j < 1) cout << "#";
+//                 }
+
+//                 else
+//                 {
+//                     if (j < m - 1) cout << ".";
+//                     else cout << (t ? "." : "#");
+//                 }
+//             }
+//         }
+//         cout << endl;
+//         t = false;
+//     }
+// }

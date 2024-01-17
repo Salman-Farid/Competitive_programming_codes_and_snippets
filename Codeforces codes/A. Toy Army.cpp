cc7999f9e4ec0d256f8af8f82600c86e8242b90b
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-01-17 15:05:22 +0600

    Problem: 152

---
+++ Codeforces codes/A. Toy Army.cpp
@@ -0,0 +1,9 @@
+// And be patient. Allah will not waste the reward of the virtuous.
+// اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n; cin>>n; cout<<n*3/2;
+}

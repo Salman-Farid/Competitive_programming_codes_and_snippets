29d3f6151a385c24c83ed5ca9366d83bf95d1dfb
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-02-13 16:08:54 +0600

    Problem: 166

--- Codeforces codes/A. Insomnia cure.cpp
+++ Codeforces codes/A. Insomnia cure.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
     long long int k,l,m,n,d,c=0; cin >> k>>l>>m>>n>>d;
-    for(int i=0;i<d; i++) if(i%k==0 || i%l==0 || i%m==0 || i%n==0) c++;
+    for(int i=1;i<=d; i++) if(i%k==0 || i%l==0 || i%m==0 || i%n==0) c++;
     cout<<c<<endl;
 }

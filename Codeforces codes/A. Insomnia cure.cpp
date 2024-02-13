8aa8c6264e6a2a54810b4d5890aaeecc7f2ba2bc
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-02-13 16:07:19 +0600

    Problem: 166

---
+++ Codeforces codes/A. Insomnia cure.cpp
@@ -0,0 +1,10 @@
+// And be patient. Allah will not waste the reward of the virtuous.
+// اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int k,l,m,n,d,c=0; cin >> k>>l>>m>>n>>d;
+    for(int i=0;i<d; i++) if(i%k==0 || i%l==0 || i%m==0 || i%n==0) c++;
+    cout<<c<<endl;
+}

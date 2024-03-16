feddf9b6d15e0c63bb8b3565c433861906a0deed
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-03-16 21:08:53 +0600

    Problem: 178

---
+++ Codeforces codes/A. Game With Sticks.cpp
@@ -0,0 +1,12 @@
+// And be patient. Allah will not waste the reward of the virtuous.
+// اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    if (min(n,m)%2!=0) cout<<"Akshat"<<endl;
+    else cout<<"Malvika"<<endl;
+}

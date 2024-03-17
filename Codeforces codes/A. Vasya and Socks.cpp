43c5243281e8899926147ff54711ddff2c0eeb3d
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-03-17 15:03:14 +0600

    Problem: 179

---
+++ Codeforces codes/A. Vasya and Socks.cpp
@@ -0,0 +1,8 @@
+// And be patient. Allah will not waste the reward of the virtuous.
+// اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
+#include <iostream>
+using namespace std;
+int main() {
+    int n,m; cin>>n>>m;
+    for(int i=1;i*m<=n;i++) n++; cout<<n;
+}

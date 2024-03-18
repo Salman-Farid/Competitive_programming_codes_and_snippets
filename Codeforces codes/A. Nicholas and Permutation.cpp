5af7a5e3d99af2007093007d5683426567e31f92
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-03-18 11:58:39 +0600

    Problem: 181

---
+++ Codeforces codes/A. Nicholas and Permutation.cpp
@@ -0,0 +1,18 @@
+// And be patient. Allah will not waste the reward of the virtuous.
+// اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n , p1,p2, l,o,w; cin >> n;
+    for(int i=1; i<=n; i++){
+        cin>> w;
+        if(w == 1) p1=i;
+        if(w==n) p2 =i;
+    }
+    l = min( p1,p2);
+    o =max(p1,p2);
+    cout<<max(n-l,o-1);
+
+}

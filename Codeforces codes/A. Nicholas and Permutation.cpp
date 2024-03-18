cb91a8450da16d3dc22f03fa3be05a95068d0ff2
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-03-18 21:39:08 +0600

    Problem: 181

--- Codeforces codes/A. Nicholas and Permutation.cpp
+++ Codeforces codes/A. Nicholas and Permutation.cpp
@@ -1,5 +1,7 @@
 // And be patient. Allah will not waste the reward of the virtuous.
 // اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
+// And be patient. Allah will not waste the reward of the virtuous.
+// اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
 
 #include <bits/stdc++.h>
 using namespace std;

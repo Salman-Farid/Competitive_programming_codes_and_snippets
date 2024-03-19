59e8fe72e49610099900efd5a278175c271be0b0
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-03-19 22:41:09 +0600

    Problem: 189

--- Codeforces codes/A. IQ test.cpp
+++ Codeforces codes/A. IQ test.cpp
@@ -5,16 +5,40 @@ using namespace std;
 int main()
 {
     int n,c=0,u=0 ; cin >> n; int arr[n];
-
     for(int j = 0; j < n; ++j) {
         cin >> arr[j];
         if (arr[j]%2==0) c+=1;
         else u+=1;
     }
-
-
     for(int i = 0; i < n; i++){
         if (c>u && arr[i]%2 != 0) cout<<i+1;
         else if (c<u && arr[i]%2 == 0) cout<<i+1;
     }
 }
+
+
+
+
+//............................................................
+// And be patient. Allah will not waste the reward of the virtuous.
+// اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n; cin >> n;
+//     int odd = 0,oddind;int even = 0,evenind;
+//     for(int i = 0;i<n;i++){
+//         int x; cin >> x;
+//         if(x%2==0){
+//             even++;
+//             evenind = i;
+//         }
+//         else{
+//             odd++;
+//             oddind = i;
+//         }
+//     }
+//     if(odd == 1) cout<<oddind+1<<endl;
+//     else cout<<evenind+1<<endl;
+// }

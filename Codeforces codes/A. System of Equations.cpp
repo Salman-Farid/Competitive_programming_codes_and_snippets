9c93870d71988261dd430d6ff1a288dbe745961e
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-03-20 20:37:26 +0600

    Problem: 185

--- Codeforces codes/A. System of Equations.cpp
+++ Codeforces codes/A. System of Equations.cpp
@@ -1,13 +1,16 @@
 // And be patient. Allah will not waste the reward of the virtuous.
 // اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
+// And be patient. Allah will not waste the reward of the virtuous.
+// اللَّهُ أَكْبَرُ //And ˹surely˺ your Lord will give so much to you that you will be pleased.
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int  n , m, b ,c=0 ; cin >> n >> m;
-    for(int a = 0; a*a <= n; a++){
-        b = n - (a*a);
-        if(m == a+(b*b)) c++;
-    }
-    cout<<c;
+    int n,l,p,y ; cin >>n;
+    l= n%7; p = n/7;y = p*2;
+    int x = ceil(p * 2) + min(l, 2);
+    if(n>=7 && l!=6) cout<< y<<" "<<x;
+    else if(n<=2)cout<<0<<" "<<n;
+    else if(n==6 or l==6) cout<<y+1<<" "<<x;
+    else cout<<0<<" "<<2;
 }

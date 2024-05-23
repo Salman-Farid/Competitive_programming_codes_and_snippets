a3bd93807aa7a4b9d82ee24392e71f02ae700db8
Salman Farid Rahman <salman.f.rahman@g.bracu.ac.bd>
2024-05-23 11:37:12 +0600

    strive dsa

---
+++ Strive DSA (A-Z) Sheet/array_medium/8.cpp
@@ -0,0 +1,19 @@
+//My approach
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        sort(nums.begin(), nums.end()); 
+        int n = nums.size(), p=1 , c = 1;  
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i + 1] == nums[i]) continue; 
+            else if (nums[i + 1] - nums[i] == 1) c++;
+            else c = 1; p = max(p, c);
+        }
+        return p;
+    }
+};
+
+
